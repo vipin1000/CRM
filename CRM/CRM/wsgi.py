f4b14d82b32311bac35aab8503c4aa228c3a81b1
vipin1000 <ruhalvipin9@gmail.com>
2025-05-21 11:28:39 +0530

    Third

--- CRM/CRM/wsgi.py
+++ CRM/CRM/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for CRM project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')
 
 application = get_wsgi_application()
+
