da22fbccb6294a0705c85b4159e8696eb377c5e6
vipin1000 <ruhalvipin9@gmail.com>
2025-06-23 18:12:15 +0530

    updated Db config

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -7,14 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See  https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-iv86#n!x-@)o*d)9_vwp0pg!=(mpu%4ptzf6w&-igd)76th(em')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_ENV', 'development') != 'production'
-
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
